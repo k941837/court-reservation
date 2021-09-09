9252d4e2fc94f312d7aa776f214a821df0890d29
k941837 <shunta2420@yahoo.co.jp>
2021-09-09 17:03:16 +0900

    ログイン機能実装。

---
+++ html/sys/login_form.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('/usr/local/lib/smarty-master/libs/Smarty.class.php');
+$smarty = new Smarty();
+
+
+//** 次の行のコメントをはずすと、デバッギングコンソールを表示します
+//$smarty->debugging = true;
+
+
+$smarty->assign('test',2222);
+
+
+$smarty->display("login_form.tpl");
+
+
+
+
+
+?>
